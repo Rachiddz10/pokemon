61663656971926adbfd1f99e1e9a543d6e81c421
rachiddz <benhammo>
2023-01-22 11:26:02 +0100

    add pokemon entity and interface to domain

--- apps/api/src/domain/interfaces.ts
+++ apps/api/src/domain/interfaces.ts
@@ -1,7 +1,18 @@
-import {Trainer} from "./entities";
+import { Trainer, Pokemon } from "./entities";
 
 export interface ITrainerRepository {
-    create(trainer: { name: string, gender: string }): Promise<Trainer>;
+  create(trainer: { name: string; gender: string }): Promise<Trainer>;
 
-    findAll(): Promise<Trainer[]>
+  findAll(): Promise<Trainer[]>;
+}
+
+export interface IPokemonRepository {
+  create(pokemon: {
+    name: string;
+    ability: string;
+    force: number;
+    trainer: Trainer;
+  }): Promise<Pokemon>;
+
+  findAll(): Promise<Pokemon[]>;
 }
