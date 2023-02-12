cc7dddc457463f168de75b7c81bc8741b1864f23
CHAU julien <kyu.shulk@gmail.com>
2023-02-12 13:51:54 +0100

    [FEAT] #8 AddPKM function first version

--- front/src/Componenets/AddPokemon.js
+++ front/src/Componenets/AddPokemon.js
@@ -1,33 +1,42 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import Axios from "axios"
 import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+const initialState = {
+  trainers: [],
+  loading: true,
+  error: false,
+  name: "",
+  pkmName: "",
+  img: "",
+  hp: "",
+  atk: "",
+  def: "",
+  atkSpe: "",
+  defSpe: "",
+  speed: "",
+  type: "",
+  chosen: false,
+  setShowAlert: false
+};
 
 export default class AddPokemon extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.state = initialState;
+    this.searchPKM = this.searchPKM.bind(this);
+    this.addPKM = this.addPKM.bind(this);
   }
 
-  state = {
-    trainers: [],
-    loading: true,
-    error: false,
-    name: "",
-    pkmName: "",
-    img: "",
-    hp: "",
-    atk: "",
-    def: "",
-    atkSpe: "",
-    defSpe: "",
-    speed: "",
-    type: "",
-    chosen: false
+  resetState() {
+    this.setState(initialState);
   }
 
   handleSubmit = async (e) => {
@@ -41,15 +50,21 @@ export default class AddPokemon extends Component {
           mode: 'no-cors',
         },
         body: JSON.stringify({
-          name: this.state.name,
-          ability: this.state.ability,
-          force: this.state.force
+          name: this.state.pkmName,
+          hp: this.state.hp,
+          atk: this.state.atk,
+          def: this.state.def,
+          atkspe: this.state.atkSpe,
+          defspe: this.state.defSpe,
+          speed: this.state.speed,
+          type: this.state.type,
+          image: this.state.img,
         })
       }
     )
       .then(response => {
         if (response.ok) {
-          this.setState({ name: "", ability: "", force: 0 }, () => {
+          this.setState({ initialState }, () => {
             console.log(this.state);
             return response.json();
           });
@@ -60,7 +75,6 @@ export default class AddPokemon extends Component {
       })
       .catch(error => this.setState({ error }, () => {
         console.log(error)
-
       }));
   };
 
@@ -104,84 +118,92 @@ export default class AddPokemon extends Component {
     })
   };
 
+  addPKM = async function (e) {
+    fetch("http://localhost:3000/pokemons",
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+          mode: 'no-cors',
+        },
+        body: JSON.stringify({
+          name: this.state.pkmName,
+          hp: this.state.hp,
+          atk: this.state.atk,
+          def: this.state.def,
+          atkspe: this.state.atkSpe,
+          defspe: this.state.defSpe,
+          speed: this.state.speed,
+          type: this.state.type,
+          image: this.state.img,
+        })
+      })
+      .then(response => {
+        if (response.ok) {
+          alert("PKM successfully added!");
+          this.setState(initialState, () => {
+            console.log(this.state);
+            return response.json();
+          });
+
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .catch(error => this.setState({ error }, () => {
+        console.log(error)
+      }));
+  }
+
   render() {
     const { trainers, loading, error } = this.state;
     return (
       <>
-        <Alert key="info" variant="info"><h2>Add Pokemon</h2> </Alert>
+        <Alert key="info" variant="info"><h2>Add Pokemon</h2></Alert>
 
         <div className="container mt-5 mb-5 d-flex justify-content-center">
-          <div className="card px-1 py-4">
-            <form className="card-body" onSubmit={this.handleSubmit}>
-              <h6 className="card-title mb-3">{" "}</h6>
-
-              <div className="row">
-                <div className="col-sm-12">
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input name="name" onChange={this.handleChange} className="form-control" type="text" placeholder="Name" defaultValue={this.state.name} /> </div>
-                </div>
+          <div className="row">
+            <div className="col d-flex align-items-stretch">
+              <div className="card px-1 py-4 h-100">
+                <form className="card-body d-flex flex-column">
+                  <h3 className="card-title text-center"></h3>
+                  <input name="name" onChange={(event => this.setState({ name: event.target.value }))} className="form-control" type="text" placeholder="Name" defaultValue={this.state.name} />
+                  <div className=" d-flex flex-column text-center px-5 mt-3 mb-3">
+                    <small className="agree-text">{" "}</small>
+                    <a href="#" className="terms">{" "}</a>
+                  </div>
+                  <button className="btn btn-info btn-block confirm-button mt-auto" type="button" onClick={this.searchPKM}>Search</button>
+                </form>
               </div>
 
-              <div className="row">
-                <div className="col-sm-12">
-                  <div className="form-group">
-                    <label htmlFor="ability">Ability</label>
-                    <input name="ability" onChange={this.handleChange} className="form-control" type="text" placeholder="Ability" defaultValue={this.state.ability} /> </div>
-                </div>
-              </div>
 
-              <div className="row">
-                <div className="col-sm-12">
-                  <div className="form-group">
-                    <label htmlFor="force">Force</label>
-                    <input name="force" onChange={this.handleChange} className="form-control" type="number" placeholder="Force" defaultValue={this.state.force} /> </div>
-                </div>
+              <div className="card px-1 py-4 h-100">
+                <form className="card-body d-flex flex-column">
+                  <div className="Display"> {
+                    !this.state.chosen ? (
+                      <h6>No PKM chosen</h6>
+                    ) : (
+                      <>
+                        <h4 className="text-center">{capitalizeFirstLetter(this.state.pkmName)}</h4>
+                        <img src={this.state.img} className="rounded mx-auto d-block img-fluid" />
+                        <h6 className="text-center">HP: {(this.state.hp)}</h6>
+                        <h6 className="text-center">ATK: {(this.state.atk)}</h6>
+                        <h6 className="text-center">DEF: {(this.state.def)}</h6>
+                        <h6 className="text-center">SP. ATK: {(this.state.atkSpe)}</h6>
+                        <h6 className="text-center">SP. DEF: {(this.state.defSpe)}</h6>
+                        <h6 className="text-center">SPEED: {(this.state.speed)}</h6>
+                        <h6 className="text-center">TYPE: {capitalizeFirstLetter(this.state.type)}</h6>
+                      </>
+                    )
+                  }
+                  </div>
+                  <button className="btn btn-info btn-block confirm-button" type="button" onClick={this.addPKM}>Add</button>
+                </form>
               </div>
-
-              <div className=" d-flex flex-column text-center px-5 mt-3 mb-3">
-                <small className="agree-text">{" "}</small>
-                <a href="#" className="terms">{" "}</a>
-              </div>
-
-              <button className="btn btn-info btn-block confirm-button">Add</button>
-            </form>
+            </div>
           </div>
-
-          <div className="card px-1 py-4">
-          <form className="card-body">
-    
-          <h3 className="card-title text-center"></h3>
-          <div className="form-group">
-          <div className="Display"> {
-            !this.state.chosen ? (
-              <h6>No PKM chosen</h6>
-            ) : (
-              <>
-              <h4 className="text-center">{capitalizeFirstLetter(this.state.pkmName)}</h4>
-              <img src={this.state.img} class="rounded mx-auto d-block img-fluid" />
-              <h6 className="text-center">HP: {(this.state.hp)}</h6>
-              <h6 className="text-center">ATK: {(this.state.atk)}</h6>
-              <h6 className="text-center">DEF: {(this.state.def)}</h6>
-              <h6 className="text-center">SP. ATK: {(this.state.atkSpe)}</h6>
-              <h6 className="text-center">SP. DEF: {(this.state.defSpe)}</h6>
-              <h6 className="text-center">SPEED: {(this.state.speed)}</h6>
-              <h6 className="text-center">TYPE: {capitalizeFirstLetter(this.state.type)}</h6>
-              </>
-            )
-          }
-          </div>
-          </div>
-          <input name="name" onChange={(event => this.setState({name: event.target.value}))} className="form-control" type="text" placeholder="Name" defaultValue={this.state.name} />
-          <div className=" d-flex flex-column text-center px-5 mt-3 mb-3">
-                <small className="agree-text">{" "}</small>
-                <a href="#" className="terms">{" "}</a>
-              </div>
-
-          <button className="btn btn-info btn-block confirm-button" type="button" onClick={this.searchPKM.bind(this)}>Search</button>
-          </form>
-          </div>
-        </div> </>
+        </div>
+      </>
     );
   }
 }
