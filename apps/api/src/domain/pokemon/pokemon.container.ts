f7830c0ae69d24c1bd16f40e66080dde70c5b3b6
AJISafae <ajib.safae@gmail.com>
2023-02-12 14:46:41 +0100

    update function

--- apps/api/src/domain/pokemon/pokemon.container.ts
+++ apps/api/src/domain/pokemon/pokemon.container.ts
@@ -1,19 +1,23 @@
 import { PokemonRepository } from "../../infrastructure/pokemon.repository";
 import { CreatePokemonUsecase } from "./create-pokemon.usecase";
 import { GetAllPokemonUsecase } from "./get-all-pokemons.usecase";
+import { UpdatePokemonUsecase } from "./update-pokemon.usecase";
 
 export type PokemonContainer = {
   createPokemonUsecase: CreatePokemonUsecase;
   getAllPokemonUsecase: GetAllPokemonUsecase;
+  updatePokemonUsecase: UpdatePokemonUsecase;
 };
 
 export const initPokemonContainer = (): PokemonContainer => {
   const pokemonRepository = new PokemonRepository();
   const createPokemonUsecase = new CreatePokemonUsecase(pokemonRepository);
   const getAllPokemonUsecase = new GetAllPokemonUsecase(pokemonRepository);
+  const updatePokemonUsecase = new UpdatePokemonUsecase(pokemonRepository);
 
   return {
     createPokemonUsecase,
     getAllPokemonUsecase,
+    updatePokemonUsecase,
   };
 };
