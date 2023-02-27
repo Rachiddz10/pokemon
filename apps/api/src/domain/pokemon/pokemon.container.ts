a1691a9d9718d5a31dc9a487502767053bb2884a
amalhamd <user.email>
2023-02-27 02:31:43 +0100

    [FEAT] #22 add filterPokemon (Backend part)

--- apps/api/src/domain/pokemon/pokemon.container.ts
+++ apps/api/src/domain/pokemon/pokemon.container.ts
@@ -4,12 +4,15 @@ import { GetAllPokemonUsecase } from "./get-all-pokemons.usecase";
 import { FindPokemonsUsecase } from "./find-pokemon.usecase";
 import { UpdatePokemonUsecase } from "./update-pokemon.usecase";
 import { DeletePokemonUsecase } from "./delete-pokemon.usecase";
+import { FilterPokemonsUsecase } from "./filter-Pokemon.usecase";
+
 export type PokemonContainer = {
   createPokemonUsecase: CreatePokemonUsecase;
   getAllPokemonUsecase: GetAllPokemonUsecase;
   findPokemonsUsecase: FindPokemonsUsecase;
   updatePokemonUsecase: UpdatePokemonUsecase;
   deletePokemonsUsecase: DeletePokemonUsecase;
+  filterPokemonsUsecase: FilterPokemonsUsecase;
 };
 
 export const initPokemonContainer = (): PokemonContainer => {
@@ -19,11 +22,13 @@ export const initPokemonContainer = (): PokemonContainer => {
   const findPokemonsUsecase = new FindPokemonsUsecase(pokemonRepository);
   const updatePokemonUsecase = new UpdatePokemonUsecase(pokemonRepository);
   const deletePokemonsUsecase = new DeletePokemonUsecase(pokemonRepository);
+  const filterPokemonsUsecase = new FilterPokemonsUsecase(pokemonRepository);
   return {
     createPokemonUsecase,
     getAllPokemonUsecase,
     findPokemonsUsecase,
     updatePokemonUsecase,
     deletePokemonsUsecase,
+    filterPokemonsUsecase,
   };
 };
